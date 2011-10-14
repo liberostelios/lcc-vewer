d32c37b1c43a123724c3c882c9056f9e3ac7ccf3
Guillaume Damiand <guillaume.damiand@liris.cnrs.fr>
2011-10-14 17:53:30 +0000

    Add the Linear_cell_complex directory by copy of the previous branch in order to conserv the history

--- Installation/auxiliary/gdb/test-gdb.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-import sys
-import os
-import gdb
-
-sys.path.insert(0, os.getcwd() + '/python')
-
-import CGAL.printers
--- Point_set_processing_3/test/Point_set_processing_3/normal_estimation_test.cmd
+++
@@ -1,1 +0,0 @@
-data/sphere926.pwn data/sphere_20k.xyz data/ChineseDragon-10kv.off
